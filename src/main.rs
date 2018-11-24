68a716e522e81e73348ad46067725ac528575695
chung tsang <cwtmyd@hotmail.com>
2018-11-24 15:26:33 -0500

    part2

---
+++ src/main.rs
@@ -0,0 +1,150 @@
+extern crate tcod;
+
+use tcod::colors::Color;
+use tcod::console::*;
+use tcod::colors;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const LIMIT_FPS: i32 = 20;
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+const COLOR_DARK_WALL: Color = Color {r:0, g:0, b:100};
+const COLOR_DARK_GROUND: Color = Color {r:50, g:50, b:150};
+
+
+
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+        Key { code: Up, ..} => player.move_by(0, -1, map),
+        Key { code: Down, ..} => player.move_by(0, 1, map),
+        Key { code: Left, ..} => player.move_by(-1, 0, map),
+        Key { code: Right, ..} => player.move_by(1, 0, map),
+        Key { code: Enter, alt: true, ..} => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        },
+        Key { code: Escape, ..} => return true,
+
+        _ => {},
+    }
+
+    false
+}
+
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+        }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
+        if !map[(self.x+dx) as usize][(self.y+dy) as usize].block_sight {
+            self.x += dx;
+            self.y += dy;
+        }
+    }
+
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile { blocked: false, block_sight: false }
+    }
+
+    pub fn wall() -> Self {
+        Tile { blocked: true, block_sight: true }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+    for object in objects {
+        object.draw(con);
+    }
+
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = map[x as usize][y as usize].blocked;
+            if wall {
+                con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    } 
+    
+    blit(con, (0,0), (MAP_WIDTH, MAP_HEIGHT), root, (0,0), 1.0, 1.0);
+}
+
+fn main() {
+    let mut root: Root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+    let mut objects = [player, npc];
+
+    while !root.window_closed() {
+        con.set_default_foreground(colors::WHITE);
+        con.clear();    
+        let map = make_map();
+        render_all(&mut root, &mut con, &objects, &map);
+        root.flush();
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut root, player, &map);
+        if exit {
+            break
+        }
+        for object in &objects {
+            object.clear(&mut con);
+        }
+    }
+
+}
