23671a9113e35e36b29c40f277ad5bde5576784e
chung tsang <cwtmyd@hotmail.com>
2018-11-28 23:58:46 -0500

    done part 3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate tcod;
+extern crate rand;
 
 use tcod::colors::Color;
 use tcod::console::*;
 use tcod::colors;
+use std::cmp;
+use rand::Rng;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -11,7 +14,9 @@ const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 const COLOR_DARK_WALL: Color = Color {r:0, g:0, b:100};
 const COLOR_DARK_GROUND: Color = Color {r:50, g:50, b:150};
-
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
 
 
 fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
@@ -88,13 +93,40 @@ impl Tile {
 }
 
 type Map = Vec<Vec<Tile>>;
-fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-    
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+fn make_map() -> (Map, (i32, i32)) {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
-    map
+    let mut starting_position = (0,0);
+    let mut rooms = vec![];
+
+    for _ in 0..MAX_ROOMS {
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE+1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE+1);
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+        let failed = rooms.iter().any(|other_room| new_room.intersects_with(other_room));
+        if !failed {
+            let (new_x, new_y) = new_room.center();
+            create_room(&new_room, &mut map);
+            if rooms.is_empty() {
+                starting_position = (new_x, new_y);
+            } else {
+                let (prev_x, prev_y) = rooms[rooms.len() -1].center();
+                if rand::random() {
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+        }
+        rooms.push(new_room);
+    }
+    
+    (map, starting_position)
 }
 
 fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
@@ -116,6 +148,49 @@ fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Ma
     blit(con, (0,0), (MAP_WIDTH, MAP_HEIGHT), root, (0,0), 1.0, 1.0);
 }
 
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect{x1: x, y1: y, x2: x+w, y2: y+h }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2) && (self.x2 >= other.x1) &&
+            (self.y1 <= other.y2) && (self.y2 >=  other.y1)
+    }
+}
+
+fn create_room (room: &Rect, map: &mut Map) {
+    for x in (room.x1+1)..room.x2 {
+        for y in (room.y1+1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2)+1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2)+1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
 fn main() {
     let mut root: Root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
@@ -127,14 +202,14 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let (map, (player_x, player_y)) = make_map();
+    let player = Object::new(player_x, player_y, '@', colors::WHITE);
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
     let mut objects = [player, npc];
 
     while !root.window_closed() {
         con.set_default_foreground(colors::WHITE);
         con.clear();    
-        let map = make_map();
         render_all(&mut root, &mut con, &objects, &map);
         root.flush();
         let player = &mut objects[0];
